1f4b56766c67ed77a3de6499450595426ab06b67
Ace <calhvoldemort@gmail.com>
2026-08-07 20:27:32 +0700

    fix bot are not move and attack

--- script/lib/include.lua
+++ script/lib/include.lua
@@ -10,25 +10,30 @@ function Include(path)
 		source = strsub(source, 2)
 	end
 	
+	-- Debug logging
+	print("[Include] Loading: " .. path .. " from: " .. source)
+	
 	if(strfind(path,"\\")) then --·ʾ
 		local pos = strfind(source,"\\script")
 		if(pos) then
 			local realpath = strsub(source,2,pos-1)..path
-			if(DEBUG) then
-				print(realpath)
-			end
-			return dofile(realpath)
+			print("[Include] Real path: " .. realpath)
+			local result = dofile(realpath)
+			print("[Include] Result: " .. tostring(result))
+			return result
 		end
+		print("[Include] ERROR: Could not find '\\script' in source")
 		return nil
 	end
 	local s,e = strfind(source,".*\\")
 	if(not e or not s) then
+		print("[Include] ERROR: Could not find end of path in source")
 		return nil
 	end
 	local realpath = strsub(source,2,e)..path
-	if(DEBUG) then
-		print(realpath)
-	end
-	return dofile(realpath)
+	print("[Include] Real path: " .. realpath)
+	local result = dofile(realpath)
+	print("[Include] Result: " .. tostring(result))
+	return result
 end
 
